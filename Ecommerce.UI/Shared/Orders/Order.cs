03d3ffa3b7bfd15c1292cd734a86edeaf21c8756
Natalia Sandalo Gomes <natalia_sandalo@hotmail.com>
2023-01-07 20:24:52 -0300

    models

---
+++ Ecommerce.UI/Shared/Orders/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.UI.Shared.Orders
+{
+    public class Order
+    {
+        public string Id { get; set; }
+        public string productId { get; set; }
+        public string currency { get; set; }
+        public int? maxPrice { get; set; }
+        public int? price { get; set; }
+        public string key { get; set; }
+    }
+}
