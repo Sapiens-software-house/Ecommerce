37479b638eb093fb3832be6d2968103e3eb4accf
Natalia Sandalo Gomes <natalia_sandalo@hotmail.com>
2023-01-08 00:56:28 -0300

    changes in products list

--- Ecommerce.UI/Shared/Product/Product.cs
+++ Ecommerce.UI/Shared/Product/Product.cs
@@ -30,12 +30,6 @@ namespace Ecommerce.UI.Shared.Product
         public Restriction restrictions { get; set; } // PEGI restrictions            
         public Requirement requirements { get; set; } // Requirements
         public Video[] videos { get; set; } // Videos            
-        public Categories[] categories { get; set; } // Categories  
-        public int? minPriceFrom { get; set; } // Minimal product's price start 
-        public int? minPriceTo { get; set; } // Minimal product's price end 
-        public string includeOutOfStock { get; set; } // Include out of stock and retail products. Default value: false, allowed values: true, false    
-        public string updatedAtFrom { get; set; } // Filter by updated at from(yyyy-mm-dd hh:mm:ss) 
-        public string updatedAtTo { get; set; } // Filter by updated at to (yyyy-mm-dd hh:mm:ss)    
-        public int? page { get; set; } // 	Number of page to get.Page size is fixed 20.Default value: 1, allowed values: 1 - 500   
+        public Categories[] categories { get; set; } // Categories            
     }
 }
