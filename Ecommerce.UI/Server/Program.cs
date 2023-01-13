3852c064b750f7725943768aa41896e5e1b66cc3
Natalia Sandalo Gomes <natalia_sandalo@hotmail.com>
2023-01-13 18:40:55 -0300

    insert UnitOfWorkPattern

--- Ecommerce.UI/Server/Program.cs
+++ Ecommerce.UI/Server/Program.cs
@@ -7,10 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
