59004181c4a771ec4d458dd9f1729f53c129f31a
Natalia Sandalo Gomes <natalia_sandalo@hotmail.com>
2023-01-13 18:56:21 -0300

    Create Repository

---
+++ Ecommerce.Infrastructure/ProductRepository/VideoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.Infrastructure.Repository;
+using Ecommerce.Interface.IProductRepository;
+using Ecommerce.Interface.Repository;
+using Ecommerce.UI.Shared.Product;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Interface.ProductRepository
+{
+    public class VideoRepository : Repository<Video>, IVideoRepository
+    {
+        public VideoRepository(DbContext context): base(context){}
+    }
+}
