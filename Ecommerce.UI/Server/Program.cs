0a08da8546bc17558c34063bcd7abbc50447c0ae
Natalia Sandalo Gomes <natalia_sandalo@hotmail.com>
2023-01-13 17:48:35 -0300

    commit adding connectstring

--- Ecommerce.UI/Server/Program.cs
+++ Ecommerce.UI/Server/Program.cs
@@ -1,10 +1,16 @@
 global using Ecommerce.UI.Shared.Product;
+using Ecommerce.Infrastructure.Data;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
