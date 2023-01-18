58a302359673baacb51306e6870005a80e80503a
Natalia Sandalo Gomes <natalia_sandalo@hotmail.com>
2023-01-17 22:48:59 -0300

    add some ids in models and some contexts

--- Ecommerce.UI/Shared/Cart/CartItem.cs
+++ Ecommerce.UI/Shared/Cart/CartItem.cs
@@ -10,7 +10,6 @@ namespace Ecommerce.UI.Shared.Cart
     {
         public int UserId { get; set; }
         public int ProductId { get; set; }
-        public int ProductTypeId { get; set; }
         public int Quantity { get; set; } = 1;
     }
 }
--- Ecommerce.UI/Shared/Orders/OrderItem.cs
+++ Ecommerce.UI/Shared/Orders/OrderItem.cs
@@ -13,7 +13,6 @@ namespace Ecommerce.UI.Shared.Orders
         public int OrderId { get; set; }
         public Product.Product Product { get; set; }
         public int ProductId { get; set; }
-        public int ProductTypeId { get; set; }
         public int Quantity { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal TotalPrice { get; set; }
