90130d17f6ae2321f2ea4366a8aab62b74c01aa9
Natalia Sandalo Gomes <natalia_sandalo@hotmail.com>
2023-02-23 16:33:43 -0300

    init layout

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Ecommerce.UI.Client;
+using Ecommerce.UI.Server;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
